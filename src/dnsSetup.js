b97c66a89e380f7d1488a2c0547b4b77f6fa4dcf
= <=>
2025-06-11 22:51:36 -0300

    v1.5.1

--- src/dnsSetup.js
+++ src/dnsSetup.js
@@ -1,14 +1,15 @@
 import fs from 'node:fs'
 
 const dnspath = 'c:/windows/system32/drivers/etc/hosts'
-
 const filedata = fs.readFileSync(dnspath).toString()
 
-if (filedata.includes('www.paypal.com')) {
-  console.log('Exists')
-  process.exit()
+function main() {
+  if (filedata.includes('www.paypal.com')) {
+    console.log('Exists')
+    return
+  }
+
+  fs.appendFileSync(dnspath, '\n127.0.0.1 www.paypal.com\n127.0.0.1 paypal.com')
+  console.log('Success')
 }
-
-fs.appendFileSync(dnspath, '\n127.0.0.1 www.paypal.com\n127.0.0.1 paypal.com')
-
-console.log('Success')
+main()
